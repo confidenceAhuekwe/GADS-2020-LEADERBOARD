c6f20bfb842d11d3b0162b5ef70232cfd00a0aa1
Confidence Ahuekwe <confidenceahuekwe@gmail.com>
2020-09-07 09:07:00 +0100

    Creating ViewPager

--- app/src/main/java/com/example/leaderboard/MainActivity.java
+++ app/src/main/java/com/example/leaderboard/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.leaderboard;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager vpPager = (ViewPager) findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(vpPager);
+
     }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 2;
+
+        public MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return LearningFragment.newInstance("Learning", "Learning Leaders");
+                case 1:
+                    return SkillFragment.newInstance("Skill IQ", "Skill IQ Leaders");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            if (position == 0) {
+                return "Learning Leaders";
+            }
+            return "Skill IQ Leaders";
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/leaderboard/SkillFragment.java
@@ -0,0 +1,67 @@
+package com.example.leaderboard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SkillFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SkillFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String title;
+    private String page;
+
+    public SkillFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+
+     * @return A new instance of fragment SkillFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SkillFragment newInstance(String page, String title) {
+        SkillFragment fragmentSkill = new SkillFragment();
+        Bundle args = new Bundle();
+        args.putString("somePage", page);
+        args.putString("someTitle", title);
+        fragmentSkill.setArguments(args);
+        return fragmentSkill;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            page = getArguments().getString("somePage");
+            title = getArguments().getString("someTitle");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_skill, container, false);
+        TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page + " -- " + title);
+        return view;
+    }
+}
